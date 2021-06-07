366e5aa0c90da8a0015cec514be9d3f68f6a6c7d
Eric Castillo <coqmos@gmail.com>
2021-06-07 13:39:32 +0000

    Merged in dev (pull request #1)
    Dev
    
    * FIrst draft
    
    * Donation form

---
+++ src/components/Donation/DonationForm.vue
@@ -0,0 +1,65 @@
+<template>
+  <form class="booking-form" id="myForm">
+    <div class="row">
+      <div class="col-lg-6 d-flex flex-column">
+        <input v-model="donationDetails.name" placeholder="Enter your first name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" class="form-control mt-20"  type="text">
+      </div>
+      <div class="col-lg-6 d-flex flex-column">
+        <input v-model="donationDetails.surname" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your surname'" class="form-control mt-20" required="" type="text">
+      </div>
+      <div class="col-lg-12 d-flex flex-column">
+        <input  v-model="donationDetails.email" name="email" placeholder="Enter email address" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" class="form-control mt-20" required="" type="email">
+      </div>
+      <div class="col-lg-12 d-flex flex-column">
+        <input v-model="donationDetails.amount"  placeholder="Donation amount (USD)" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Donation amount (USD)'" class="form-control mt-20" required="" type="text">
+
+        <textarea class="form-control mt-20" name="message" placeholder="Messege" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Messege'" required="" v-model="donationDetails.message"></textarea>
+      </div>
+
+      <div class="col-lg-12 d-flex justify-content-end send-btn">
+        <span class="submit-btn primary-btn mt-20 text-uppercase " v-on:click="donationClick">donate<span class="lnr lnr-arrow-right"></span></span>
+      </div>
+      <div class="alert-msg"></div>
+    </div>
+  </form>
+</template>
+
+<script>
+import {RepositoryFactory} from "@/repositories/RepositoryFactory";
+
+export default {
+  name: "DonationForm",
+  data() {
+    return {
+      donationDetails: {
+        name: null,
+        surname: null,
+        email: null,
+        amount: null,
+        message: null,
+      },
+    }
+  },
+  methods: {
+    donationClick() {
+      RepositoryFactory
+          .get('donation')
+          .postDonation(this.donationDetails)
+          .then(
+              response => {
+                this.$emit('donationCreated', response.data.data)
+              }
+          )
+          .catch(
+              error => {
+                this.$emit('hasError', error);
+              }
+          )
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Errors.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row d-flex justify-content-end">
+    <div class="col-lg-12 col-sm-12 pb-80 header-text">
+      <h1>Donation Error</h1>
+      <p>
+        There seemed to have been an error in starting the donation process.
+      </p>
+      <p>
+        Not to worry no money was taken at this point. Please try again later.
+      </p>
+      <p class="p-red">
+        Error Message: {{errorMessage}} <br/>
+      </p>
+      <p class="p-red" v-if="errorMessage.response">
+        Errors
+      <ul>
+        <li v-for="(item, index) in errorMessage.response.data.errors" :key="index">
+          {{ index }} - {{ item }}
+        </li>
+      </ul>
+      </p>
+      <p>
+            <span class="btn btn-black" v-on:click="reload">
+              Reload
+            </span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Errors",
+  props: {
+    errorMessage: {
+      type: Object,
+      default: null
+    }
+  },
+  methods: {
+    reload() {
+      location.reload();
+    },
+  }
+}
+</script>
